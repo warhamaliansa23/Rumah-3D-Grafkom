a28ff0efa55494d3fa7bb9c755fe5d403aefd990
ilhamode <odeilham144@gmail.com>
2021-12-27 10:37:12 +0700

    Perubahan bagian Ilham

--- Rumah 3D Grafkom.cpp
+++ Rumah 3D Grafkom.cpp
@@ -57,6 +57,19 @@ z1,float z2,float z3){
 // bagian Hadijah Nisa Ifayatin_E1E120008
 
 // bagian La Ode Muhamad Ilham_E1E120030
+void cobaini(int x1,int y1,int x2,int y2,int x3,int y3,int x4,int
+y4,float z1,float z2,float z3){//atap
+glColor3f(1,0,1);//belakang
+     glVertex3d(x4,y4,z1);
+     glVertex3d(x3,y3,z1);
+     glVertex3d(x3,y1,z3);
+     glVertex3d(x4,y1,z3);
+     glColor3f(1,0,1);//depan
+     glVertex3d(x3,y3,z2);
+     glVertex3d(x4,y4,z2);
+     glVertex3d(x4,y1,z3);
+     glVertex3d(x3,y1,z3);
+}
 
 // bagian Ita Puspita _E1E120072
 
